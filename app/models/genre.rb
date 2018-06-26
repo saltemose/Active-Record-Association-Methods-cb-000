e8334cc4a8c5b6e6fdf45ae002de2fc27c975e6d
Sean Altemose <sean@seanaltemose.com>
2018-06-26 15:15:42 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.names
+    self.artists.name
   end
 
     # return an array of strings containing every musician's name
